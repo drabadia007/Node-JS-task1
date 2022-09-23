79ca560439bbe5ed64700d4f87f8de75cacd67b8
20IT118 <80442907+drabadia007@users.noreply.github.com>
2022-09-23 21:47:33 +0530

    Add files via upload

---
+++ Node JS Lab/index.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const calc = require("./calc");
+fs.readFile("input.txt", { encoding: "utf8", flag: "r" }, function (err, data) {
+  if (err) return console.error(err);
+  else {
+    let result = 0;
+    const lines = data.split(/\r?\n/);
+
+    for (let i = 0; i < lines.length; i += 3) {
+      result = calc(lines[i], lines[i + 1], lines[i + 2]);
+      console.log(result);
+      result = result.toString() + "\n";
+      fs.writeFileSync("output.txt", result, { flag: "a+" });
+    }
+  }
+});
